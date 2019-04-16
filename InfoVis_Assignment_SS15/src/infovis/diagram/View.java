529b65c5947427ebe2823ec823f5fcf8993b692b
MaximilianMargraf <m.margraf.m@googlemail.com>
2019-04-16 14:42:57 +0200

    assignment 1, task 1 finished

---
+++ InfoVis_Assignment_SS15/src/infovis/diagram/View.java
@@ -0,0 +1,113 @@
+package infovis.diagram;
+
+import infovis.diagram.elements.Element;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Rectangle2D;
+import java.util.Iterator;
+
+import javax.swing.JPanel;
+
+
+
+public class View extends JPanel{
+	//
+	private final static int  MAX_WIDTH  = 800;
+	private final static int MAX_HEIGHT = 720;
+
+	private Model model = null;
+	private Color color = Color.BLUE;
+	private double scale = 1;
+	private double translateX= 0;
+	private double translateY=0;
+	private Rectangle2D marker = new Rectangle2D.Double();
+	private Rectangle2D overviewRect = new Rectangle2D.Double();   
+
+	public Model getModel() {
+		return model;
+	}
+	public void setModel(Model model) {
+		this.model = model;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	
+	public void paint(Graphics g) {
+		
+		Graphics2D g2D = (Graphics2D) g;
+		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		g2D.clearRect(0, 0, getWidth(), getHeight());
+
+		// use scale member to zoom in and out.
+		g2D.scale(getScale(), getScale());
+
+		// paint the scene like it is
+		paintDiagram(g2D);
+
+		// adjust size of graphic object for the overview. This window will be 1/4 the size of the normal scene
+		g2D.scale(0.25 / getScale(), 0.25/getScale());
+
+		// size of the rectangle equals the resized graphic object
+		overviewRect.setRect(0,0, getWidth(), getHeight());
+
+		// clean area over/under the rectangle
+		g2D.clearRect(0,0, getWidth(), getHeight());
+
+		// set frame color
+		g2D.setColor(Color.BLACK);
+
+		// draw rectangle
+		g2D.draw(overviewRect);
+
+		// draw the resized scene on its place
+		paintDiagram(g2D);
+	}
+
+
+	private void paintDiagram(Graphics2D g2D){
+		for (Element element: model.getElements()){
+			element.paint(g2D);
+		}
+	}
+	
+	public void setScale(double scale) {
+		this.scale = scale;
+	}
+	public double getScale(){
+		return scale;
+	}
+	public double getTranslateX() {
+		return translateX;
+	}
+	public void setTranslateX(double translateX) {
+		this.translateX = translateX;
+	}
+	public double getTranslateY() {
+		return translateY;
+	}
+	public void setTranslateY(double tansslateY) {
+		this.translateY = tansslateY;
+	}
+	public void updateTranslation(double x, double y){
+		setTranslateX(x);
+		setTranslateY(y);
+	}
+	public void updateMarker(int x, int y){
+		marker.setRect(x, y, 16, 10);
+	}
+	public Rectangle2D getMarker(){
+		return marker;
+	}
+	public boolean markerContains(int x, int y){
+		return marker.contains(x, y);
+	}
+}
+ 
